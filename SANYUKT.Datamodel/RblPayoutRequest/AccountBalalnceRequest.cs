42ab1e64756fe72722e7ee8f89b48c22513405a1
ajaiverma1986 <45514244+ajaiverma1986@users.noreply.github.com>
2024-06-07 17:02:52 +0530

    202406070

---
+++ SANYUKT.Datamodel/RblPayoutRequest/AccountBalalnceRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SANYUKT.Datamodel.RblPayoutRequest
+{
+    public class AccountBalalnceRequest
+    {
+        public GetAccountBalanceReq getAccountBalanceReq { get; set; }
+    }
+    public class Body
+    {
+        public string AcctId { get; set; }
+    }
+
+    public class GetAccountBalanceReq
+    {
+        public Header Header { get; set; }
+        public Body Body { get; set; }
+        public Signature1 Signature { get; set; }
+    }
+
+    public class Header
+    {
+        public string TranID { get; set; }
+        public string Corp_ID { get; set; }
+        public string Approver_ID { get; set; }
+    }
+    public class Signature1
+    {
+        public string Signature { get; set; }
+    }
+
+}
---
+++ SANYUKT.Datamodel/RblPayoutResponse/RblBalanceResponse.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SANYUKT.Datamodel.RblPayoutResponse
+{
+    public class BalAmt
+    {
+        public string amountValue { get; set; }
+        public CurrencyCode currencyCode { get; set; }
+    }
+
+    public class Body
+    {
+        public BalAmt BalAmt { get; set; }
+    }
+
+    public class CurrencyCode
+    {
+    }
+
+    public class GetAccountBalanceRes
+    {
+        public Header Header { get; set; }
+        public Body Body { get; set; }
+        public Signature1 Signature { get; set; }
+    }
+
+    public class Header
+    {
+        public string TranID { get; set; }
+        public string Corp_ID { get; set; }
+        public string Approver_ID { get; set; }
+        public string Status { get; set; }
+        public string Error_Cde { get; set; }
+        public string Error_Desc { get; set; }
+    }
+
+    public class BalanceResponse
+    {
+        public GetAccountBalanceRes getAccountBalanceRes { get; set; }
+    }
+
+    public class Signature1
+    {
+        public string Signature { get; set; }
+    }
+}
