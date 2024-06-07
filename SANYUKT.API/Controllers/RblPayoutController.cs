c1b2e31860020f6aa24cb67247e24269ad892a3d
ajaiverma1986 <45514244+ajaiverma1986@users.noreply.github.com>
2024-06-07 17:56:04 +0530

    202406070000

---
+++ SANYUKT.API/Controllers/RblPayoutController.cs
@@ -0,0 +1,49 @@
+﻿using Audit.WebApi;
+using Microsoft.AspNetCore.Mvc;
+using SANYUKT.API.Common;
+using SANYUKT.API.Security;
+using SANYUKT.Datamodel.DTO.Request;
+using SANYUKT.Datamodel.DTO.Response;
+using SANYUKT.Datamodel.RblPayoutRequest;
+using SANYUKT.Datamodel.Shared;
+using SANYUKT.Provider;
+using SANYUKT.Provider.Payout;
+using System.Threading.Tasks;
+
+namespace SANYUKT.API.Controllers
+{
+    [ResponseCache(Duration = -1, Location = ResponseCacheLocation.None, NoStore = true)]
+    [ServiceFilter(typeof(SANYUKTExceptionFilterService))]
+    public class RblPayoutController: BaseApiController
+    {
+        public readonly RblPayoutProvider _Provider;
+        
+        public RblPayoutController()
+        {
+            _Provider = new RblPayoutProvider();
+        }
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <param name="userLoginRequest"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AuditApi(EventTypeName = "POST RblPayoutController/GetAccountBalalance", IncludeHeaders = true, IncludeResponseBody = true, IncludeRequestBody = false, IncludeModelState = false)]
+        public async Task<IActionResult> GetAccountBalalance([FromBody] AccountBalalnceRequest request)
+        {
+        //    UserLoginResponse response = new UserLoginResponse();
+
+        //    ErrorResponse error = await _callValidator.AuthenticateAndAuthorize(this.CallerUser, false);
+        //    if (error.HasError)
+        //    {
+        //        response.SetError(error);
+        //        return Ok(response);
+        //    }
+        SimpleResponse response1 =new SimpleResponse();
+            response1 = await _Provider.GetBalalce(request, this.CallerUser);
+
+            return Ok(response1);
+
+        }
+    }
+}
--- SANYUKT.Provider/Payout/RblPayoutProvider.cs
+++ SANYUKT.Provider/Payout/RblPayoutProvider.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SANYUKT.Commonlib.Cache;
+using SANYUKT.Datamodel.Common;
+using SANYUKT.Datamodel.Entities.Authorization;
+using SANYUKT.Datamodel.Interfaces;
+using SANYUKT.Datamodel.RblPayoutRequest;
+using SANYUKT.Datamodel.Shared;
+using SANYUKT.Provider.Shared;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
 
 namespace SANYUKT.Provider.Payout
 {
-    public class RblPayoutProvider
+    public class RblPayoutProvider:BaseProvider
     {
+        public async Task<SimpleResponse> GetBalalce(AccountBalalnceRequest requestreq, ISANYUKTServiceUser serviceUser)
+        {
+            SimpleResponse response=new SimpleResponse ();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, "https://apideveloper.rblbank.com/test/sb/rbl/v1/accounts/balance/query?client_id=5b6aa5e8e2ca77ba9af0c19305663f98&client_secret=c1ae9fe7e68451ee53e25fa2e4bb4c69");
+            string username = "FINDOORPAY";
+            string pass = "Findoor@12@#";
+            var authenticationString = $"{username}:{pass}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(authenticationString));
+            request.Headers.Add("Authorization", $"Basic {base64EncodedAuthenticationString}");
+
+            //make the request
+            string jsonstr= JsonConvert.SerializeObject(requestreq, Formatting.Indented);
+            var content =new  StringContent(jsonstr);
+            request.Content = content;
+            var response1 = await client.SendAsync(request);
+           response.Result= await response1.Content.ReadAsStringAsync();
+
+            return response;
+        }
     }
+    
 }
